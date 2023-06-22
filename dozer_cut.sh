ac16ddce5b564174d0801fc01fb483d18dc772ee
NAKCHEOL <ncseong@sphinfo.co.kr>
2023-06-22 12:00:59 +0900

    Add files via upload

---
+++ dozer_cut.sh
@@ -0,0 +1,5 @@
+#/bin/bash
+source /opt/conda/etc/profile.d/conda.sh
+conda activate base
+cd /ebim/route
+python process.py --execute_type all --equip_type dozer --input_path "./input/sample_simple_2.7x3.csv"  --input_file_type csv --start_block_name BL_9  --work_direction 1 --e 10 --s 3 --l 15 --allowable_error_height 0.05 --sub_type cut --output_file_type all
