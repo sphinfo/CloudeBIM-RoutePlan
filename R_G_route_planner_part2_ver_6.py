4b1fcd9340388779bddc67bd98eb8181238878f6
NAKCHEOL <ncseong@sphinfo.co.kr>
2024-08-12 09:51:49 +0900

    v8 적용

--- R_G_route_planner_part2_ver_6.py
+++ R_G_route_planner_part2_ver_6.py
@@ -16,7 +16,13 @@ def read_csv_files(file):
     df1 = df[['x1', 'y1']].rename(columns={'x1': 'x', 'y1': 'y'})
     df2 = df[['x2', 'y2']].rename(columns={'x2': 'x', 'y2': 'y'})
 
-    return df1, df2
+    mid_x = (df1['x'] + df2['x']) / 2
+    mid_y = (df1['y'] + df2['y']) / 2
+
+    # 중간 점을 이용한 df0 생성
+    df0 = pd.DataFrame({'x': mid_x, 'y': mid_y})
+
+    return df1, df2, df0
 
 # 두 점 거리 구하는 함수
 def calculate_distances(df1, df2):
@@ -28,6 +34,16 @@ def calculate_distances(df1, df2):
         distance = np.sqrt((x1 - x2)**2 + (y1 - y2)**2)
         distances.append(distance)
     return distances
+
+# 두 점 거리 구하는 함수(중심 선)
+def calculate_min_distances_center_node(df0):
+    distances = []
+    for i in range(len(df0)-1):
+        x1, y1 = df0.iloc[i]['x'], df0.iloc[i]['y']
+        x2, y2 = df0.iloc[i+1]['x'], df0.iloc[i+1]['y']
+        distance = np.sqrt((x1 - x2)**2 + (y1 - y2)**2)
+        distances.append(distance)
+    return min(distances)
 
 # 도로 폭이 가장 클 때, 중복도 구하기
 def first_repeated_rate(model_width, attachment_width, equipment_width, safety_line, x_min):
@@ -119,23 +135,6 @@ def second_move_points_in_parallel(df1, df2, second_gap):
         new_points.append(new_point)
     return pd.DataFrame(new_points, columns=['x', 'y'])
 
-# 장비 시작지점 설정 함수
-# True : 1 -> 2, False : 2 -> 1
-def set_starting_point(num1):
-    if num1=="1" :
-        return True
-    else :
-        return False
-
-# 장비 작업 방향 설정 함수
-# True : A -> B, False : B -> A
-def set_starting_direction(A):
-    if A=='A' :
-        return True
-    else :
-        return False
-    
-
 # 장애물 검사 함수    
 def check_obstacle(obstacles, line_num, df1_length):
     """Check if there is an obstacle on the specified line."""
@@ -146,6 +145,38 @@ def check_obstacle(obstacles, line_num, df1_length):
             else:
                 return False  # 장애물의 인덱스가 df1의 개수보다 크면 장애물이 없는 것으로 간주
     return False  # 장애물이 없는 경우
+
+# 장비가 들어갈 공간
+def set_equipment_space(equipment_length, min_node_dist) :
+    space = math.ceil(equipment_length/min_node_dist)
+    return space
+
+# 라인 변경에 필요한 최소 노드 개수
+def get_requierd_dist_for_line_change_num(line_change_way, min_node_dist, turning_radius):
+    
+    required_dist_for_line_change = 0
+    if line_change_way == 1 : # 1:후진 후 변경
+        required_dist_for_line_change = turning_radius * 1.45
+    elif line_change_way == 2 : # 2:후진 중 변경
+        required_dist_for_line_change = 0
+    else : # 3: 삼점 회전법
+        required_dist_for_line_change = turning_radius
+    
+    node_num = math.ceil(required_dist_for_line_change/min_node_dist)
+    
+    return node_num
+
+# 작업구역 선택
+def set_work_area(df1,df2,start_line, end_line):
+    df1_none_work_area_1 = df1.iloc[:start_line]
+    df1_work_area = df1.iloc[start_line:end_line]
+    df1_none_work_area_2 = df1.iloc[end_line:]
+    
+    df2_none_work_area_1 = df2.iloc[:start_line]
+    df2_work_area = df2.iloc[start_line:end_line]
+    df2_none_work_area_2 = df2.iloc[end_line:]
+
+    return df1_none_work_area_1, df1_work_area, df1_none_work_area_2, df2_none_work_area_1, df2_work_area, df2_none_work_area_2
 
 # 라인 변경 함수
 def line_change(line1, line2):
@@ -197,6 +228,7 @@ def main():
     # input 값 입력하기
     equipment_width = args['equipment_width']
     attachment_width = args['attachment_width']
+    equipment_length = args["equipment_length"]
     safety_line = args['safety_line']
     x_min = args['x_min']
     turning_radius = args['turning_radius']
@@ -204,43 +236,68 @@ def main():
     starting_direction = args['starting_direction']
     input_file = args['input_file']
     output_file = args['output_file']
-    df1, df2 = read_csv_files(input_file)
+    start_line = args['start_line']
+    end_line = args['end_line']
+    
+    df1, df2, df0 = read_csv_files(input_file)
     
     cycle_num = args['cycle_num'] # 싸이클 횟수
     line_change_way = args['line_change_way'] # 1:후진 후 변경, 2:후진 중 변경, 3:3점회전법
 
     # working_type = "rolling" # rolling : 다짐, grading : 평탄화, fill : 성토 등등
     
-    equipment = 'roller' # roller & grader
-    
-    # 필요한 최소 후진거리
-    required_distance_for_backward = 0
-    if line_change_way == 1 :
-        required_distance_for_backward = turning_radius * 1.5
-    elif line_change_way == 2 :
-        required_distance_for_backward = 0
-    else : 
-        required_distance_for_backward = turning_radius
+    # 시작 위치가 B->A 일 경우 df1과 df2, df0 역순으로 정렬
+    if starting_direction == "B" :
+        df1 = df1.iloc[::-1].reset_index(drop=True)
+        df2 = df2.iloc[::-1].reset_index(drop=True)
+        df0 = df0.iloc[::-1].reset_index(drop=True)
 
-    node_distance = 2 # 노드 사이 거리
+    min_node_dist = calculate_min_distances_center_node(df0) # 도로 중심선 노드 사이 거리 최소값
+
+    space = set_equipment_space(equipment_length, min_node_dist) # 장비가 들어갈 노드 개수
     
-    # 후진에 필요한 노드 수
-    required_points = math.ceil(required_distance_for_backward / node_distance)
+    requierd_dist_for_line_change_num = get_requierd_dist_for_line_change_num(line_change_way, min_node_dist, turning_radius) # 라인변경에 필요한 최소 노드 수
+    min_start_line = requierd_dist_for_line_change_num + space # start_line 최소값
+    
+    if line_change_way == 3:
+        max_end_line = len(df1)-requierd_dist_for_line_change_num # end_line 최대값    
+    else :
+        max_end_line = len(df1)
+
+    # start_line 범위 초과 시 범위 내로 수정
+    if int(start_line) > int(end_line) :
+        start_line = int(end_line) - 1
+    
+    if int(start_line) < int(min_start_line) :
+        start_line = int(min_start_line)  
+   
+    
+    # end_line 범위 초과 시 범위 내로 수정
+    if int(end_line)>int(max_end_line) : 
+        end_line = int(max_end_line)
+
+    if int(end_line) < int(start_line) :
+        end_line = int(start_line) + 1
+    
+    # df_none_work_area_1 : start_line 전의 구역
+    # df_work_area : start_line과 end_line 사이의 구역
+    # df_none_work_area_2 : end_line 이후의 구역
+    df1_none_work_area_1, df1_work_area, df1_none_work_area_2, df2_none_work_area_1, df2_work_area, df2_none_work_area_2 = set_work_area(df1, df2, start_line, end_line)            
+    
+    df1_none_work_area_1 = df1_none_work_area_1.iloc[space:]
+    df2_none_work_area_1 = df2_none_work_area_1.iloc[space:]
+
+    df1_work_area = pd.concat([df1_none_work_area_1, df1_work_area])
+    df2_work_area = pd.concat([df2_none_work_area_1, df2_work_area]) 
+
+    # working_type = "rolling" # rolling : 다짐, grading : 평탄화, fill : 성토 등등
+    equipment = 'roller' # roller & grader    
 
     # 장애물 위치 설정 (원한다면 추가도 가능)
     obstacles = [
     [2, 20],
     [3, 10]
     ]
-
-    # 시작 위치, 방향 설정
-    start_option = set_starting_point(starting_position)
-    direction_option = set_starting_direction(starting_direction)
-
-    # 시작 위치가 B->A 일 경우 df1과 df2 역순으로 정렬
-    if direction_option == False :
-        df1 = df1.iloc[::-1].reset_index(drop=True)
-        df2 = df2.iloc[::-1].reset_index(drop=True)
 
     distances = calculate_distances(df1, df2)
     
@@ -267,9 +324,9 @@ def main():
     # 각 라인에 대한 평행 이동
     for i in range(exact_y):
         if i == 0:  # 첫번째 라인
-            forward_line = first_move_points_in_parallel(df1, df2, first_gap/2, safety_line)
+            forward_line = first_move_points_in_parallel(df1_work_area, df2_work_area, first_gap/2, safety_line)
         else:  # 이후의 라인들
-            forward_line = second_move_points_in_parallel(forward_line, df2, second_gap)
+            forward_line = second_move_points_in_parallel(forward_line, df2_work_area, second_gap)
 
         forward_lines[i] = forward_line
 
@@ -289,7 +346,7 @@ def main():
         if current_cycle % 2 == 0:
             # 라인 번호 할당
             for j in range(exact_y):
-                if start_option == True: # 라인 방향 1->2
+                if starting_position == 1: # 라인 방향 1->2
                     forward_waypoints[current_cycle][j] = forward_lines[j]
                     backward_waypoints[current_cycle][j] = backward_lines[j]
                 else : # 라인 방향 2->1
@@ -298,7 +355,7 @@ def main():
         
         else : # 짝수번째 싸이클
             for j in range(exact_y):
-                if start_option == True: # 라인 방향 1->2
+                if starting_position == 1: # 라인 방향 1->2
                     forward_waypoints[current_cycle][j] = forward_lines[exact_y - j - 1]
                     backward_waypoints[current_cycle][j] = backward_lines[exact_y - j - 1]
                 else : # 라인 방향 2->1
@@ -323,9 +380,9 @@ def main():
                 
 
                 # Slice the DataFrames
-                first_part_forward_line = current_line_df.iloc[:required_points]
-                second_part_forward_line = current_line_df.iloc[required_points:]
-                first_part_previous_forward_line = previous_line_df.iloc[:required_points]
+                first_part_forward_line = current_line_df.iloc[:requierd_dist_for_line_change_num]
+                second_part_forward_line = current_line_df.iloc[requierd_dist_for_line_change_num:]
+                first_part_previous_forward_line = previous_line_df.iloc[:requierd_dist_for_line_change_num]
 
                 # Apply the line_change function between the previous and current lines
                 new_forward_line = line_change(first_part_previous_forward_line, first_part_forward_line)
@@ -351,8 +408,6 @@ def main():
                     new_backward_line = line_change(current_line_waypoints, next_line_waypoints)
                     backward_waypoints[current_cycle][j] = new_backward_line
     
-
-
     # 마지막 사이클의 마지막 후진 경로 삭제
     last_cycle = cycle_num - 1
     if last_cycle in backward_waypoints and len(backward_waypoints[last_cycle]) > 0:
