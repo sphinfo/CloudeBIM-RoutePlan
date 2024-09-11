16345d87a2988c1177ae909fa588c36345516987
NAKCHEOL <ncseong@sphinfo.co.kr>
2024-09-11 15:33:13 +0900

    9.9 외곽선 sorted파일 출력

--- cell_create.sh
+++ cell_create.sh
@@ -8,4 +8,6 @@ attachment_width=${4}
 equipment_length=${5}
 starting_position=${6}
 starting_direction=${7}
-python /data2/ebim/execute/routev2/cell_create.py --input_file $input_file --output_file $output_file --equipment_width $equipment_width --attachment_width $attachment_width --starting_position $starting_position --starting_direction $starting_direction
+repeated_rate=${8}
+output_newline_file=${9}
+python /data2/ebim/execute/routev2/cell_create.py --input_file $input_file --output_file $output_file --equipment_width $equipment_width --attachment_width $attachment_width --starting_position $starting_position --starting_direction $starting_direction --repeated_rate $repeated_rate --output_newline_file $output_newline_file
