be75f0c2624de5dc43ea40863aeca86763262c4e
NAKCHEOL <ncseong@sphinfo.co.kr>
2025-02-24 12:21:51 +0900

    24년 최종

--- route_planner_v10/util.py
+++ route_planner_v10/util.py
@@ -2,9 +2,10 @@
 import os
 import csv
 import pyodbc # type: ignore
-import geojson
+import geojson # type: ignore
 import time
-import numpy as np
+import logging
+import numpy as np # type: ignore
 from functools import wraps
 
 
@@ -18,7 +19,7 @@ def get_data(cur, table_name):
 
 
 
-def read_mdb(full_path, table_name):
+def read_mdb(full_path, table_name: str | list):
     con, cur, result = None, None, []
     try:
         if not os.path.exists(full_path):
@@ -40,7 +41,7 @@ def read_mdb(full_path, table_name):
 
 def read_csv(full_path):
     if not os.path.exists(full_path):
-        raise Exception(f'File not found, {full_path}')
+        raise FileNotFoundError(f'File not found, {full_path}')
 
     rows = []
     with open(full_path, 'r') as csvfile:
@@ -50,22 +51,16 @@ def read_csv(full_path):
 #TO SPH geojosn reader
 def read_geojson(full_path):
     if not os.path.exists(full_path):
-        raise Exception(f'File not found, {full_path}')
+        raise FileNotFoundError(f'File not found, {full_path}')
 
     rows = []
     with open(full_path, 'r') as jsonfile:
         reader = geojson.loads(jsonfile.read())
         features = reader['features']
         for feature in features:
-            etc = ''
-            coordinates = feature.geometry.coordinates
-            for parts in coordinates:
-                for coordinate in parts:
-                    etc += str(coordinate[0])+","+str(coordinate[1])+",0"
-                etc+";"
-            feature.properties["etc"] = etc
             rows.append(feature.properties)
     return rows
+
 #TO SPH min_dist연산
 def calculate_min_dist_center_node(linelist):
     distances = []
@@ -94,10 +89,55 @@ def log_decorator(log_type):
     def wrapper(func):
         @wraps(func)
         def decorator(*args, **kwargs):
-            import logging
             start = time.time()
             result = func(*args, **kwargs)
             logging.info(f'{log_type} 완료.. 소요시간: {time.time() - start}초')
             return result
         return decorator
     return wrapper
+
+
+def calculate_s_num(j: int, s: int, distances: list):
+    cumulative_distance = 0
+
+    for cell_num in range(0, len(distances)):
+        try:
+            cumulative_distance += distances[j + cell_num - 1]
+        except IndexError:
+            logging.warning(f'calculate_s_num distances index out of range, j: {j}, s: {s}, cell_num: {cell_num}, len(distances): {len(distances)}')
+            # raise IndexError(f'calculate_s_num distances index out of range, j: {j}, s: {s}, cell_num: {cell_num}, len(distances): {len(distances)}')
+            return cell_num
+        
+        if cumulative_distance >= s:
+            cell_num += 1
+            break
+        
+    return cell_num
+
+def calculate_space(j: int, dist: int, distances: list):
+    cell_num, cumulative_distance = 1, 0
+    for cell_num in range(1, j):
+        try:
+            cumulative_distance += distances[j - 1 - cell_num]
+        except IndexError:
+            logging.warning(f'calculate_space distances index out of range, j: {j}, dist: {dist}, cell_num: {cell_num}, len(distances): {len(distances)}')
+            # raise IndexError(f'calculate_space distances index out of range, j: {j}, dist: {dist}, cell_num: {cell_num}, len(distances): {len(distances)}')
+            return cell_num
+
+        if cumulative_distance >= dist:
+            break
+    return cell_num
+
+
+def calculate_h_num(j: int, required_line_change_distance: int, distances: list):
+    return calculate_space(j, required_line_change_distance, distances)
+
+
+def dist_node(node: dict, next_node):
+    x1, y1 = node['x'], node['y']
+    x2, y2 = next_node['x'], next_node['y']
+    return np.sqrt((x1 - x2)**2 + (y1 - y2)**2)
+
+
+def dist_each_node(df0: list):
+    return [dist_node(df0[i], df0[i + 1]) for i in range(len(df0) - 1)]
