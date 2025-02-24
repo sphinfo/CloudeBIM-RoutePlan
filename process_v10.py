efb414ffd5e1f6892e1566d270771edfa2fb21cd
NAKCHEOL <ncseong@sphinfo.co.kr>
2025-02-24 12:12:20 +0900

    Update process_v10.py

--- process_v10.py
+++ process_v10.py
@@ -1,32 +1,47 @@
 
 # COPYRIGHT ⓒ 2024 HANYANG UNIVERSITY. ALL RIGHTS RESERVED.
+
+
 import time
 import logging
 from route_planner_v10.arguments import args
-from route_planner_v10.constants import TABLE_NAMES
-from route_planner_v10.util import read_mdb, read_csv, read_geojson, file_name, calculate_min_dist_center_node
+from route_planner_v10.util import read_geojson, read_csv
 from route_planner_v10.block import Block
 from route_planner_v10.alloc import DozerAlloc
 from route_planner_v10.plan import DozerRoutePlan
+from route_planner_v10.plot import draw, draw_obstacle
+from route_planner_v10.exception import RouteCreationError
 
+def execute_dozer(args: dict):
+    output_file, visual_mode, input_line_file, input_cell_file = map(
+        args.get, ['output_file', 'visual_mode',
+                   'input_line_file', 'input_cell_file'])
 
-def execute_dozer(data: dict, input_file_name: str, execute_type: str):
-    param, block_items, outline_data = Block.valid_parameter(*list(map(data.get, TABLE_NAMES)))
-    
+    param, block_items, outline_data = Block.valid_parameter(
+        args=args, cell_data=read_geojson(input_cell_file), model_line_data=read_csv(input_line_file))
+
     # 할당셀
     dozer = DozerAlloc(block_items, outline_data)
     allocate_cell, allocate_cell_names, allocate_outline_cell = dozer.alloc(param)
 
-    # 할당셀 결과 저장
-    #dozer.save_output_csv(input_file_name)
-    
-    if execute_type == 'alloc': return
     # 계획경로
     dozer_route = DozerRoutePlan(block_items, allocate_cell, allocate_outline_cell, outline_data, allocate_cell_names)
-    route_plan = dozer_route.calc_route_plan(param)
-
+    route_plan, intersected_blocks, intersected_block_names = dozer_route.calc_route_plan(param)
+    
     # 계획 경로 결과 저장
-    dozer_route.save_output_csv(args['output_file'])
+    dozer_route.save_output_csv(output_file)
+
+    # 장애물 교차
+    if intersected_block_names:
+        if visual_mode:
+            draw_obstacle(block_items, route_plan, intersected_blocks, param)
+        raise RouteCreationError('Buffer lines must not overlap with other obstacle cells, Block: ' + ','.join(intersected_block_names))
+
+    # visual
+    if visual_mode:
+        draw(block_items, route_plan)
+
+
 
 # python3.12 process_v10.py --input_path "./input/20240715_test.MDB"
 # python3.12 process_v10.py --input_path "./input/20240715_test.MDB" --execute_type alloc
@@ -35,20 +50,14 @@ if __name__ == "__main__":
     logging.info(f'cmd: python process.py --{" --".join([k + " " + str(v) for k, v in args.items()])}')
 
     start = time.time()
-    # Input_Parameter, Cell_Data
-    #data = read_mdb(args.get('input_path'), TABLE_NAMES)
-    #TO SPH 입력 Parameter model_line = csv file
-    #                     cell_data = json file
-    #                     input_parameter = argument
-    data = {}
-    data["Model_line_Data"] = read_csv(args['input_line_file'])
-    Min_Cendist = calculate_min_dist_center_node(data["Model_line_Data"])
-    args["Min_Cendist"] = Min_Cendist
-    data["Input_Parameter"] = [args]
-    data["Cell_Data"] = read_geojson(args['input_cell_file'])
 
-    logging.info(f'Read {args.get("input_path")}.. duration: {time.time() - start} sec')
+    data = {
+        'Model_line_Data': read_csv(args['input_line_file']),
+        'Input_Parameter': [args],
+        'Cell_Data': read_geojson(args['input_cell_file'])
+    }
 
-    execute_dozer(data, file_name(args.get("input_path")), args.get("execute_type"))
+    execute_dozer(args)
 
     logging.info(f'total duration: {time.time() - start} sec')
+    
