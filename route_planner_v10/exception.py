6717253bd7c8524a919953efda691058003b3d46
NAKCHEOL <ncseong@sphinfo.co.kr>
2025-02-24 12:20:11 +0900

    24년 최종

--- route_planner_v10/exception.py
+++ route_planner_v10/exception.py
@@ -1,1 +1,15 @@
 # COPYRIGHT ⓒ 2024 HANYANG UNIVERSITY. ALL RIGHTS RESERVED.
+
+
+class InputDataError(Exception):
+    """Exception raised for input data error."""
+
+    def __init__(self, message: str):
+        self.message = message
+
+
+class RouteCreationError(Exception):
+    """Exception raised for route plan creation error."""
+
+    def __init__(self, message: str):
+        self.message = message
