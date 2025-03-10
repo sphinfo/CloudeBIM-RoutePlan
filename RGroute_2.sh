59590d6647eb48639de278d9c03b10f5ffbb0fab
NAKCHEOL <ncseong@sphinfo.co.kr>
2025-03-10 11:25:32 +0900

    24년 최종

--- RGroute_2.sh
+++ RGroute_2.sh
@@ -15,4 +15,13 @@ line_change_way=${11}
 equipment_length=${12}
 start_line=${13}
 end_line=${14}
-python /data2/ebim/execute/routev2/R_G_route_planner_part2_ver_6.py --input_file $input_file --output_file $output_file --equipment_width $equipment_width --attachment_width $attachment_width --safety_line $safety_line --x_min $x_min --turning_radius $turning_radius --starting_position $starting_position --starting_direction $starting_direction --cycle_num $cycle_num --line_change_way $line_change_way --equipment_length $equipment_length --start_line $start_line --end_line $end_line
+obstacles=${15}
+
+if [ -z $obstacles ]
+then
+obstacles=""
+else
+obstacles="--obstacles $obstacles"
+fi
+
+python /data2/ebim/execute/routev2/R_G_route_planner_part2_ver_6.py --input_file $input_file --output_file $output_file --equipment_width $equipment_width --attachment_width $attachment_width --safety_line $safety_line --x_min $x_min --turning_radius $turning_radius --starting_position $starting_position --starting_direction $starting_direction --cycle_num $cycle_num --line_change_way $line_change_way --equipment_length $equipment_length --start_line $start_line --end_line $end_line $obstacles
