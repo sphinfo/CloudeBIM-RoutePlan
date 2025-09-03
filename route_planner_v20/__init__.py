a54502e7bcc93afcc41cd5757b8fc460f0da4e65
NAKCHEOL <ncseong@sphinfo.co.kr>
2025-09-03 10:41:01 +0900

    0703 수정요청

--- route_planner_v20/__init__.py
+++ route_planner_v20/__init__.py
@@ -1,18 +1,18 @@
 
-# COPYRIGHT ⓒ 2025 HANYANG UNIVERSITY. ALL RIGHTS RESERVED.
+# COPYRIGHT ⓒ 2024 HANYANG UNIVERSITY. ALL RIGHTS RESERVED.
 import logging
 from os import makedirs
 from datetime import datetime
-from route_planner_v20.util import file_name
-from route_planner_v20.arguments import args
-from route_planner_v20.version import __version__
-from route_planner_v20.constants import LOGGING_LEVEL, LOGGING_PATH, LOGGING_FLAG
+from route_planner_v10.util import file_name
+from route_planner_v10.arguments import args
+from route_planner_v10.version import __version__
+from route_planner_v10.constants import LOGGING_LEVEL, LOGGING_PATH, LOGGING_FLAG
 
 VERSION = __version__
 
 
 print(f'Route Planner Module.. version: {VERSION}')
-print('COPYRIGHT ⓒ 2025 HANYANG UNIVERSITY. ALL RIGHTS RESERVED.')
+print('COPYRIGHT ⓒ 2024 HANYANG UNIVERSITY. ALL RIGHTS RESERVED.')
 
 if LOGGING_FLAG:
     LOGGING_MAP = {
@@ -22,12 +22,12 @@ if LOGGING_FLAG:
         'INFO': logging.INFO,
         'DEBUG': logging.DEBUG,
     }
-    logging_path = args['logging_path'] if args.get('logging_path') else LOGGING_PATH
+
     MSG_FORMAT = '%(asctime)s %(levelname)s %(name)s: %(message)s'
     DATETIME_FORMAT = '%Y-%m-%d %H:%M:%S'
-    makedirs(logging_path, exist_ok=True)
-
-    file_handler = logging.FileHandler(filename=f'{logging_path}/{datetime.now().strftime("%Y%m%d%H%M%S")}.log', encoding='utf-8')
+    makedirs(LOGGING_PATH, exist_ok=True)
+    makedirs(f'{LOGGING_PATH}/{file_name(args.get("output_file"))}', exist_ok=True)
+    file_handler = logging.FileHandler(filename=f'{LOGGING_PATH}/{file_name(args.get("output_file"))}/{datetime.now().strftime("%Y%m%d%H%M%S")}.log', encoding='utf-8')
     file_handler.setFormatter(logging.Formatter(fmt=MSG_FORMAT, datefmt=DATETIME_FORMAT))
     logger = logging.getLogger()
     logger.setLevel(LOGGING_MAP.get(LOGGING_LEVEL))
