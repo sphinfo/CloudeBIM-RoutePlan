96b66684de075b042fef0429092603bac205e4a0
NAKCHEOL <ncseong@sphinfo.co.kr>
2025-09-03 10:44:04 +0900

    0703 수정요청

--- route_planner_v10/arguments.py
+++ route_planner_v10/arguments.py
@@ -9,16 +9,23 @@ parser.add_argument('--Blade_Capacity', type=float, required=True, help='블레�
 parser.add_argument('--Blade_Width', type=float, required=True, help='블레이드 폭')
 parser.add_argument('--Equipment_Width', type=float, required=True, help='장비 폭')
 parser.add_argument('--needed_dist', type=float, required=False, default=2.5, help='라인변경에 필요한 거리(상수 변경예정)')
-parser.add_argument('--required_line_change_distance', type=float, required=False, default='2.5', help='라인변경에 필요한 거리')
-parser.add_argument('--Repeated_rate', type=float, required=False, help='중복도')
+
+# v1.3.0, 25.06.22 required True로 변경 (알고리즘 상에서 grader가 아닌 경우에도 사용하므로)
+parser.add_argument('--Repeated_rate', type=float, required=True, help='중복도')
+
 parser.add_argument('--Starting_Direction', type=str, required=False, help='작업 시작 방향')
 parser.add_argument('--Starting_Point', type=str, required=False, help='작업 진행 방향')
-parser.add_argument('--Min_Fwdist', type=float, required=False, default=5.0, help='최소 전진 거리(상수 변경예정)')
+# 25.03.21
+# parser.add_argument('--Min_Fwdist', type=float, required=False, default=5.0, help='최소 전진 거리(상수 변경예정)')
+parser.add_argument('--Min_Fwdist', type=float, required=True, default=5.0, help='최소 전진 거리(상수 변경예정)')
 parser.add_argument('--Min_Cendist', type=float, required=False, help='중심선 노드간 최소거리(내부 셀에서 연산)')
 parser.add_argument('--Obstacle_Cell', type=str, required=False, help='장애물 셀 지정')
 parser.add_argument('--Start_Line', type=int, required=True, help='시작라인')
 
 # v1.1.0
+# 25.03.21
+# parser.add_argument('--required_line_change_distance', type=float, required=True, default='-', help='라인변경에 필요한 거리')
+parser.add_argument('--required_line_change_distance', type=float, required=False, default='2.5', help='라인변경에 필요한 거리')
 parser.add_argument('--equipment_length', type=float, required=True, help='장비 길이')
 parser.add_argument('--equipment', type=str, required=False,  default='dozer', choices=['dozer', 'grader'], help='장비 종류,')
 parser.add_argument('--end_line', type=int, required=False, help='종료 라인')
@@ -26,6 +33,11 @@ parser.add_argument('--turning_radius', type=float, required=False, help='회전
 parser.add_argument('--safety_line_df1', type=float, required=False, default=0, help='좌측 안전 거리')
 parser.add_argument('--safety_line_df2', type=float, required=False, default=0, help='우측 안전 거리')
 parser.add_argument('--visual_mode', required=False, default=False, action=argparse.BooleanOptionalAction, help='Debugging Visual ON')
- 
+
+# v1.3.0
+# 25.06.22
+parser.add_argument('--grader_front_length', type=float, required=False, help='그레이더 전방길이')
+parser.add_argument('--grader_rear_length', type=float, required=False, help='그레이더 후방길이')
+
 
 args = {k: v for k, v in parser.parse_args().__dict__.items() if v is not None}
